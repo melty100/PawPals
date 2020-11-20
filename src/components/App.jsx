adad640d3f16d7b0ddc8f029a1483e1f077494da
Daniel Beltran <dannybv92@gmail.com>
2020-11-19 21:19:06 -0800

    added jsx files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Reacr from 'react';
+import logo from '../login.svg';
+import './App.scss';
+import { Login } from '../components/login/register';
+
+function App() {
+    return (
+        <div className='App'>
+            <Login/>
+        </div>
+    );
+}
+
+export default App;
